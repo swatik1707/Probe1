19260ee7dc15ddd1f5075fec66797d99da43e974
Ruslan Gubaydullin <Gubaidullin.r@bk.ru>
2020-05-10 14:30:13 +0400

    added Import architecture

--- OS_version.py
+++ OS_version.py
@@ -1,7 +1,8 @@
 import sys
+import platform
 
 info = 'OS info is \n{}\n\nPython version is {}'.format(
-    sys.platform, sys.version)
+    sys.platform, sys.version, platform.architecture())
 print(info)
 
 with open('os_info', 'w') as ff:
